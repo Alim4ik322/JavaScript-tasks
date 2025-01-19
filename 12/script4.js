23eec87e52740f0f1fdec45145d3192bc08a52f3
Alim4ik322 <assanovali23@gmail.com>
2025-01-19 21:55:04 +0300

    задание 13

---
+++ 12/script4.js
@@ -0,0 +1,8 @@
+let num; 
+num = 123;   
+alert(num);
+let a;
+a = 10;   
+alert(a);
+a = 20;   
+alert(a);
---
+++ 13/script14(1).js
@@ -0,0 +1,7 @@
+let a = 1;
+alert(a);
+let b = 2, c=2;
+alert(b,c);
+let d;
+d=3;
+alert(d);
